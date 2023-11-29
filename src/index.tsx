d733c39182bec5974d5893d43f67cf7364f715d1
JUNU <bokjunwoo@gmail.com>
2023-11-29 22:26:17 +0900

    feat: 기본 App render 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#F2F2F2',
+    },
+  },
+  typography: {
+    fontFamily: 'Noto Sans KR',
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +21,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
