107752dad2ccb6bb17a33baba18327a1a9341996
JUNU <bokjunwoo@gmail.com>
2023-12-14 21:24:07 +0900

    style: ExpImage 컴포넌트에서 이미지 경로 수정

--- src/components/ui/ExpImage.tsx
+++ src/components/ui/ExpImage.tsx
@@ -5,7 +5,7 @@ type ExpImageType = {
 };
 
 const ExpImage = ({ src }: ExpImageType) => {
-  return <Avatar variant="rounded" src={src} />;
+  return <Avatar variant="rounded" src={`./images/${src}`} />;
 };
 
 export default ExpImage;
