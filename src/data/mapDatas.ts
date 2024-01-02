9be504801f1c0a8748d4018fa1fcf1a201b22440
JUNU <bokjunwoo@gmail.com>
2024-01-02 23:59:48 +0900

    feat: 기본 맵 데이터 추가

---
+++ src/data/mapDatas.ts
@@ -0,0 +1,178 @@
+import { AraneRiverMapDataType, GrandisMapDataType } from './mapTypes';
+
+export const AraneRiverMapData: AraneRiverMapDataType = {
+  소멸의여로: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  리버스시티: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  츄츄아일랜드: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  얌얌아일랜드: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  레헬른: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  아르카나: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  모라스: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  에스페라: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  셀라스: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  문브릿지: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  고통의미궁: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  리멘: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+};
+
+export const GrandisMapData: GrandisMapDataType = {
+  세르니움: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  불타는세르니움: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  호텔아르크스: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  오디움: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  도원경: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  아르테리아: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+  카르시온: [
+    {
+      id: 1,
+      map_name: '',
+      monster: '',
+      number_of_monster: 0,
+      monster_experience: 0,
+    },
+  ],
+};
---
+++ src/data/mapTypes.ts
@@ -0,0 +1,32 @@
+type MapType = {
+  id: number;
+  map_name: string;
+  monster: string;
+  number_of_monster: number;
+  monster_experience: number;
+};
+
+export type AraneRiverMapDataType = {
+  소멸의여로: MapType[];
+  리버스시티: MapType[];
+  츄츄아일랜드: MapType[];
+  얌얌아일랜드: MapType[];
+  레헬른: MapType[];
+  아르카나: MapType[];
+  모라스: MapType[];
+  에스페라: MapType[];
+  셀라스: MapType[];
+  문브릿지: MapType[];
+  고통의미궁: MapType[];
+  리멘: MapType[];
+};
+
+export type GrandisMapDataType = {
+  세르니움: MapType[];
+  불타는세르니움: MapType[];
+  호텔아르크스: MapType[];
+  오디움: MapType[];
+  도원경: MapType[];
+  아르테리아: MapType[];
+  카르시온: MapType[];
+};
