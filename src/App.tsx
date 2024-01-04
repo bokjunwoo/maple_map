1badf270138d45ef4568f0c088f846bc7d92e247
JUNU <bokjunwoo@gmail.com>
2024-01-04 23:59:54 +0900

    style: MapTable 컴포넌트 import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AppLayout from './components/ui/Layout/AppLayout';
 import ExpSetting from './components/ExpSetting';
+import MapTable from './components/MapTable';
 
 const App = () => {
   return (
     <AppLayout>
       <ExpSetting />
+      <MapTable />
     </AppLayout>
   );
 };
