50e57d8a0e9fb7e27006aa2b75ff66ccf4460b5e
JUNU <bokjunwoo@gmail.com>
2024-01-06 23:59:30 +0900

    style: TableSortHead import

--- src/components/MapTable.tsx
+++ src/components/MapTable.tsx
@@ -2,23 +2,16 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { AraneRiverMapData } from '../data/mapDatas';
+import TableSortHead from './Table/TableSortHead';
 
 export default function MapTable() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>맵 이름</TableCell>
-            <TableCell align="center">몬스터</TableCell>
-            <TableCell align="center">몬스터 젠수</TableCell>
-            <TableCell align="center">몬스터 경험치</TableCell>
-          </TableRow>
-        </TableHead>
+        <TableSortHead />
         <TableBody>
           {AraneRiverMapData.소멸의여로.map((row) => (
             <TableRow
