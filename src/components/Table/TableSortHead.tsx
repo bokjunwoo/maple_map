8bc0c96cbdddbdae3fd67bf678f958ea58528f5d
JUNU <bokjunwoo@gmail.com>
2024-01-07 23:46:15 +0900

    refactor: TableSortHead에서 headCells 사용하도록 변경

--- src/components/Table/TableSortHead.tsx
+++ src/components/Table/TableSortHead.tsx
@@ -1,30 +1,14 @@
-import { useState } from 'react';
 import { TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
 import { MapType } from '../../data/mapTypes';
+import { headCells } from '../../data/mapDatas';
 
-type HeadCell = {
-  id: keyof MapType;
-  label: string;
-  disableSorting?: boolean;
+type TableSortHeadType = {
+  orderBy: keyof MapType;
+  order: 'desc' | 'asc';
+  handleChange: (property: keyof MapType) => void;
 };
 
-const headCells: HeadCell[] = [
-  { id: 'map_name', label: '맵이름', disableSorting: true },
-  { id: 'monster', label: '몬스터', disableSorting: true },
-  { id: 'number_of_monster', label: '몬스터 젠수' },
-  { id: 'monster_experience', label: '경험치' },
-];
-
-const TableSortHead = () => {
-  const [orderBy, setOrderBy] = useState<keyof MapType>('id');
-  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
-
-  const handleSortRequest = (property: keyof MapType) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
+const TableSortHead = ({ orderBy, order, handleChange }: TableSortHeadType) => {
   return (
     <TableHead>
       <TableRow>
@@ -40,7 +24,7 @@ const TableSortHead = () => {
               <TableSortLabel
                 active={orderBy === headCell.id}
                 direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={() => handleSortRequest(headCell.id)}
+                onClick={() => handleChange(headCell.id)}
               >
                 {headCell.label}
               </TableSortLabel>
