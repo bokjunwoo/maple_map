cfa1c8b87b0f6d49994a2dfd65c957984c1db7a3
JUNU <bokjunwoo@gmail.com>
2024-01-09 23:59:53 +0900

    style: MapSelect import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import AppLayout from './components/ui/Layout/AppLayout';
+import AppLayout from './components/UI/Layout/AppLayout';
 import ExpSetting from './components/ExpSetting';
 import MapTable from './components/MapTable';
+import MapSelect from './components/MapSelect';
 
 const App = () => {
   return (
     <AppLayout>
       <ExpSetting />
+      <MapSelect />
       <MapTable />
     </AppLayout>
   );
