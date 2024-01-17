84f82505ccf57df557217e91f3b5d94a07690650
JUNU <bokjunwoo@gmail.com>
2024-01-17 23:05:29 +0900

    feat: 각 맵별로 색상 데이터 추가

---
+++ src/data/colorDatas.ts
@@ -0,0 +1,25 @@
+type SymbolsColorType = {
+  [key: string]: { bgColor: string };
+};
+
+export const symbolsColor: SymbolsColorType = {
+  소멸의여로: { bgColor: '#ebf1fa' },
+  리버스시티: { bgColor: '#ebeded' },
+  츄츄아일랜드: { bgColor: '#f7f2f9' },
+  얌얌아일랜드: { bgColor: '#fff0f0' },
+  레헬른: { bgColor: '#f7f2f9' },
+  아르카나: { bgColor: '#ebf1fa' },
+  모라스: { bgColor: '#fff0f0' },
+  에스페라: { bgColor: '#f6f8ee' },
+  셀라스: { bgColor: '#ebf1fa' },
+  문브릿지: { bgColor: '#ebf1fa' },
+  고통의미궁: { bgColor: '#f6f8ee' },
+  리멘: { bgColor: '#ebeded' },
+  세르니움: { bgColor: '#ebf1fa' },
+  불타는세르니움: { bgColor: '#fff0f0' },
+  호텔아르크스: { bgColor: '#f7f2f9' },
+  오디움: { bgColor: '#f6f8ee' },
+  도원경: { bgColor: '#ebf1fa' },
+  아르테리아: { bgColor: '#ebeded' },
+  카르시온: { bgColor: '#f7f2f9' },
+};
