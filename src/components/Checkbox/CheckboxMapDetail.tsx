6f719ddef9af361475a96bd6b5918351098fdffa
JUNU <bokjunwoo@gmail.com>
2024-01-17 23:07:04 +0900

    feat: 각 맵별 디자인 업데이트

--- src/components/Checkbox/CheckboxMapDetail.tsx
+++ src/components/Checkbox/CheckboxMapDetail.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Box, Chip } from '@mui/material';
+import { symbolsColor } from '../../data/colorDatas';
 
 type CheckboxMapDetailType = {
   name: string;
@@ -15,6 +16,8 @@ const CheckboxMapDetail = ({
     onToggle(name);
   };
 
+  const backgroundColor = symbolsColor[name].bgColor;
+
   return (
     <Box onClick={handleClick}>
       <Chip
@@ -23,11 +26,21 @@ const CheckboxMapDetail = ({
           boxSizing: 'border-box',
           border: isSelected ? '1.5px solid blue' : 'none',
           color: isSelected ? 'blue' : 'black',
-          bgcolor: 'skyblue',
+          bgcolor: { backgroundColor },
           marginRight: 1,
           marginBottom: 1,
+          pl: 0.8,
+          '& .MuiChip-label': {
+            ml: -0.3,
+          },
         }}
-        avatar={<Avatar>M</Avatar>}
+        avatar={
+          <Avatar
+            src={`./images/map_icon/${name}.png`}
+            variant="rounded"
+            sx={{ p: 0.15 }}
+          />
+        }
         label={name}
         variant={isSelected ? 'outlined' : 'filled'}
       />
