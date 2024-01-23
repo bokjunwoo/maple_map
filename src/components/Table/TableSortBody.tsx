c681dd57ffa499aa23d56d9ffc889d30de7f3b41
JUNU <bokjunwoo@gmail.com>
2024-01-23 23:41:11 +0900

    refactor: TableDataRow 컴포넌트로 세분화작업

--- src/components/Table/TableSortBody.tsx
+++ src/components/Table/TableSortBody.tsx
@@ -1,15 +1,8 @@
-import {
-  TableBody,
-  TableRow,
-  TableCell,
-  SelectChangeEvent,
-} from '@mui/material';
-import { headCells } from '../../data/mapDatas';
+import { TableBody } from '@mui/material';
 import { MapType } from '../../data/mapTypes';
 import EmptyDataRowUI from './TableUI/EmptyDataRowUI';
-import SelectCellUI from './TableUI/SelectCellUI';
-import SwitchCellUI from './TableUI/SwitchCellUI';
 import { SetStateAction } from 'react';
+import TableDataRow from './TableDataRow';
 
 type TableSortBodyType = {
   data: MapType[];
@@ -23,45 +16,12 @@ const TableSortBody = ({ data, onUpdate }: TableSortBodyType) => {
         <EmptyDataRowUI />
       ) : (
         data.map((row) => (
-          <TableRow key={row.map_name}>
-            {headCells.map((headCell) => (
-              <TableCell
-                key={headCell.id}
-                align={headCell.id !== 'map_name' ? 'center' : 'inherit'}
-              >
-                {headCell.id === 'rune' ? (
-                  <SwitchCellUI
-                    checked={row.rune}
-                    onChange={(value) => {
-                      const updatedData = data.map((map) =>
-                        map.map_name === row.map_name
-                          ? { ...map, rune: value }
-                          : map
-                      );
-                      onUpdate(updatedData);
-                    }}
-                  />
-                ) : headCell.id === 'burning_field' ? (
-                  <SelectCellUI
-                    value={row.burning_field}
-                    onChange={(event: SelectChangeEvent<number>) => {
-                      const updatedData = data.map((map) =>
-                        map.map_name === row.map_name
-                          ? {
-                              ...map,
-                              burning_field: Number(event.target.value),
-                            }
-                          : map
-                      );
-                      onUpdate(updatedData);
-                    }}
-                  />
-                ) : (
-                  row[headCell.id]
-                )}
-              </TableCell>
-            ))}
-          </TableRow>
+          <TableDataRow
+            data={data}
+            item={row}
+            onUpdate={onUpdate}
+            key={row.map_name}
+          />
         ))
       )}
     </TableBody>
