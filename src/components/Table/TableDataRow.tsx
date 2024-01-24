799f9a811e902d474809ef0325365134b06c2326
JUNU <bokjunwoo@gmail.com>
2024-01-24 23:55:09 +0900

    feat: 각 행의 셀에서 행을 펼치고 접는 기능 구현

--- src/components/Table/TableDataRow.tsx
+++ src/components/Table/TableDataRow.tsx
@@ -1,10 +1,18 @@
-import { TableCell, SelectChangeEvent, TableRow } from '@mui/material';
+import {
+  TableCell,
+  SelectChangeEvent,
+  TableRow,
+  IconButton,
+} from '@mui/material';
 import { headCells } from '../../data/mapDatas';
 import { MapType } from '../../data/mapTypes';
 import SelectCellUI from './TableUI/SelectCellUI';
 import SwitchCellUI from './TableUI/SwitchCellUI';
-import { SetStateAction } from 'react';
+import { SetStateAction, useState } from 'react';
 import { symbolsColor } from '../../data/colorDatas';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import MapDetailCollapseUI from './TableUI/MapDetailCollapseUI';
 
 type TableDataRowType = {
   data: MapType[];
@@ -13,6 +21,8 @@ type TableDataRowType = {
 };
 
 const TableDataRow = ({ data, item, onUpdate }: TableDataRowType) => {
+  const [open, setOpen] = useState(false);
+
   const handleSwitchChange = (value: boolean) => {
     const updatedData = data.map((map) =>
       map.map_name === item.map_name ? { ...map, rune: value } : map
@@ -31,25 +41,34 @@ const TableDataRow = ({ data, item, onUpdate }: TableDataRowType) => {
   const backgroundColor = symbolsColor[item.map_key].bgColor;
 
   return (
-    <TableRow sx={{ bgcolor: backgroundColor }}>
-      {headCells.map((headCell) => (
-        <TableCell
-          key={headCell.id}
-          align={headCell.id !== 'map_name' ? 'center' : 'inherit'}
-        >
-          {headCell.id === 'rune' ? (
-            <SwitchCellUI checked={item.rune} onChange={handleSwitchChange} />
-          ) : headCell.id === 'burning_field' ? (
-            <SelectCellUI
-              value={item.burning_field}
-              onChange={handleSelectChange}
-            />
-          ) : (
-            item[headCell.id]
-          )}
+    <>
+      <TableRow sx={{ bgcolor: backgroundColor }}>
+        <TableCell>
+          <IconButton onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
         </TableCell>
-      ))}
-    </TableRow>
+
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.id !== 'map_key' ? 'center' : 'inherit'}
+          >
+            {headCell.id === 'rune' ? (
+              <SwitchCellUI checked={item.rune} onChange={handleSwitchChange} />
+            ) : headCell.id === 'burning_field' ? (
+              <SelectCellUI
+                value={item.burning_field}
+                onChange={handleSelectChange}
+              />
+            ) : (
+              item[headCell.id]
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+      <MapDetailCollapseUI open={open} mapInfo={item} />
+    </>
   );
 };
 
