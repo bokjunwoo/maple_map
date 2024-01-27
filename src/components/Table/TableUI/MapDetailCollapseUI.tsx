a09ace29380f0d4d078d4c4c80833184cb9576bc
JUNU <bokjunwoo@gmail.com>
2024-01-27 23:38:58 +0900

    feat: CardMedia 이미지 기능 추가

--- src/components/Table/TableUI/MapDetailCollapseUI.tsx
+++ src/components/Table/TableUI/MapDetailCollapseUI.tsx
@@ -1,4 +1,11 @@
-import { TableRow, TableCell, Collapse, Box, Typography } from '@mui/material';
+import {
+  TableRow,
+  TableCell,
+  Collapse,
+  Box,
+  Card,
+  CardMedia,
+} from '@mui/material';
 import { MapType } from '../../../data/mapTypes';
 
 type MapDetailCollapseUIType = {
@@ -9,12 +16,22 @@ type MapDetailCollapseUIType = {
 const MapDetailCollapseUI = ({ open, mapInfo }: MapDetailCollapseUIType) => {
   return (
     <TableRow>
-      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
         <Collapse in={open} timeout="auto" unmountOnExit>
           <Box sx={{ margin: 1 }}>
-            <Typography variant="h6" gutterBottom component="div">
-              {mapInfo.monster}
-            </Typography>
+            <Card sx={{ display: 'flex', border: 'none', boxShadow: 'none' }}>
+              <CardMedia
+                component="img"
+                sx={{
+                  width: 460,
+                  height: 230,
+                  objectFit: 'fill',
+                  borderRadius: '4px',
+                }}
+                image="/images/test1.png"
+                alt="Live from space album cover"
+              />
+            </Card>
           </Box>
         </Collapse>
       </TableCell>
