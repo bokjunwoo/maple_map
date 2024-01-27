2b7b8716a17327f9526d4d1ebe9f52c295b97beb
JUNU <bokjunwoo@gmail.com>
2024-01-27 23:41:46 +0900

    feat: CardContent 기능 추가

--- src/components/Table/TableUI/MapDetailCollapseUI.tsx
+++ src/components/Table/TableUI/MapDetailCollapseUI.tsx
@@ -3,8 +3,11 @@ import {
   TableCell,
   Collapse,
   Box,
+  Typography,
   Card,
   CardMedia,
+  CardContent,
+  Avatar,
 } from '@mui/material';
 import { MapType } from '../../../data/mapTypes';
 
@@ -31,6 +34,64 @@ const MapDetailCollapseUI = ({ open, mapInfo }: MapDetailCollapseUIType) => {
                 image="/images/test1.png"
                 alt="Live from space album cover"
               />
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ p: 0, width: 300 }}>
+                  <Box sx={{ display: 'flex', alignItems: 'center', m: 1 }}>
+                    <Avatar
+                      src={`./images/map_icon/소멸의여로.png`}
+                      variant="rounded"
+                      sx={{ p: 0.15 }}
+                    />
+                    <Typography variant="h6" marginLeft={0.7}>
+                      {mapInfo.map_name}
+                    </Typography>
+                  </Box>
+
+                  <Box sx={{ display: 'flex', alignItems: 'center', m: 1 }}>
+                    <Avatar
+                      src={`./images/map_icon/소멸의여로.png`}
+                      variant="circular"
+                      sx={{ p: 0.15, width: 24, height: 24 }}
+                    />
+                    <Typography variant="subtitle1" marginLeft={0.5}>
+                      {mapInfo.symbols}
+                    </Typography>
+                  </Box>
+
+                  <Box sx={{ display: 'flex', alignItems: 'center', m: 1 }}>
+                    <Avatar
+                      src={`./images/map_icon/소멸의여로.png`}
+                      variant="circular"
+                      sx={{ p: 0.15, width: 24, height: 24 }}
+                    />
+                    <Typography variant="subtitle2" marginLeft={0.5}>
+                      {mapInfo.monster}
+                    </Typography>
+                  </Box>
+
+                  <Box sx={{ display: 'flex', alignItems: 'center', m: 1 }}>
+                    <Avatar
+                      src={`./images/map_icon/소멸의여로.png`}
+                      variant="circular"
+                      sx={{ p: 0.15, width: 24, height: 24 }}
+                    />
+                    <Typography variant="subtitle2" marginLeft={0.5}>
+                      {mapInfo.monster_experience}
+                    </Typography>
+                  </Box>
+
+                  <Box sx={{ display: 'flex', alignItems: 'center', m: 1 }}>
+                    <Avatar
+                      src={`./images/map_icon/소멸의여로.png`}
+                      variant="circular"
+                      sx={{ p: 0.15, width: 24, height: 24 }}
+                    />
+                    <Typography variant="subtitle2" marginLeft={0.5}>
+                      {mapInfo.monster_money}
+                    </Typography>
+                  </Box>
+                </CardContent>
+              </Box>
             </Card>
           </Box>
         </Collapse>
