f30136ed4a0960722580459a2eb9e99e5d937677
JUNU <bokjunwoo@gmail.com>
2024-01-30 23:30:13 +0900

    refactor: 카드 구조를 단순화

--- src/components/Card/CardMapDetail.tsx
+++ src/components/Card/CardMapDetail.tsx
@@ -1,15 +1,9 @@
-import {
-  Box,
-  CardMedia,
-  CardContent,
-  Avatar,
-  Typography,
-  Card,
-  TextField,
-} from '@mui/material';
+import { Box, CardMedia, CardContent, Card } from '@mui/material';
 import CardAvatar from './CardUI/CardAvatarUI';
 import { MapType } from '../../data/mapTypes';
 import { ChangeEvent, useState } from 'react';
+import CardValueWithLabelUI from './CardUI/CardValueWithLabelUI';
+import CardTextFieldWithLabelUI from './CardUI/CardTextFieldWithLabelUI';
 
 type CardMapDetailType = {
   mapInfo: MapType;
@@ -40,113 +34,75 @@ const CardMapDetail = ({ mapInfo }: CardMapDetailType) => {
 
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <CardContent sx={{ p: 0, ml: 1, width: 250 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-            <Avatar
-              src={`./images/map_icon/${mapInfo.map_key}.png`}
-              variant="rounded"
-              sx={{ p: 0.15 }}
-            />
-            <Typography variant="h6" marginLeft={0.5}>
-              {mapInfo.map_name}
-            </Typography>
-          </Box>
+          <CardAvatar
+            src={`./images/map_icon/${mapInfo.map_key}.png`}
+            variant="h6"
+            text={mapInfo.map_name}
+            size={35}
+          />
 
           <CardAvatar
             src={`/images/symbol_icon/${mapInfo.map_key}.png`}
             variant="subtitle1"
             text={mapInfo.symbols}
+            size={24}
           />
 
           <CardAvatar
             src={`./images/monster.png`}
             variant="subtitle2"
             text={mapInfo.monster}
+            size={24}
           />
 
           <CardAvatar
             src={`./images/exp.png`}
             variant="subtitle2"
             text={mapInfo.monster_experience}
+            size={24}
           />
 
           <CardAvatar
             src={`./images/meso.png`}
             variant="subtitle2"
             text={mapInfo.monster_money}
+            size={24}
           />
         </CardContent>
       </Box>
 
       <Box>
         <CardContent sx={{ p: 0, ml: 1 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-            <Avatar
-              src={`./images/map_icon/${'미정'}.png`}
-              variant="rounded"
-              sx={{ p: 0.15 }}
-            />
-            <Typography variant="h6" marginLeft={0.5}>
-              상세정보 계산기
-            </Typography>
-          </Box>
+          <CardAvatar
+            src={`./images/map_icon/소멸의여로.png`}
+            variant="h6"
+            text="상세정보 계산기"
+            size={35}
+          />
 
-          <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, mb: 1 }}>
-            <Typography variant="subtitle1" marginLeft={0.5}>
-              젠당 잡을 수 있는 마릿수:
-            </Typography>
+          <CardTextFieldWithLabelUI
+            label="젠당 잡을 수 있는 마릿수:"
+            value={monsterValue}
+            onChange={handleMonsterValue}
+            maxValue={mapInfo.number_of_monster}
+          />
 
-            <Box sx={{ display: 'flex', alignItems: 'center' }} marginLeft={1}>
-              <TextField
-                sx={{ width: 40 }}
-                id="outlined-size-small"
-                size="small"
-                defaultValue={0}
-                value={monsterValue}
-                onChange={handleMonsterValue}
-                inputProps={{ sx: { p: 0.5, textAlign: 'center' } }}
-              />
+          <CardTextFieldWithLabelUI
+            label="6분당 내가 잡는 마릿수:"
+            value={sixMinutesMonsterValue}
+            onChange={handleMonsterValue}
+            maxValue={mapInfo.number_of_monster * 48}
+          />
 
-              <Typography variant="subtitle1" marginLeft={0.5}>
-                / {mapInfo.number_of_monster}
-              </Typography>
-            </Box>
-          </Box>
+          <CardValueWithLabelUI
+            label="6분당 경험치 획득량:"
+            value={sixMinutesMonsterValue * mapInfo.monster_experience}
+          />
 
-          <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, mb: 1 }}>
-            <Typography variant="subtitle1" marginLeft={0.5}>
-              6분당 내가 잡는 마릿수:
-            </Typography>
-
-            <Box sx={{ display: 'flex', alignItems: 'center' }} marginLeft={1}>
-              <TextField
-                sx={{ width: 40 }}
-                id="outlined-size-small"
-                size="small"
-                defaultValue={0}
-                value={sixMinutesMonsterValue}
-                onChange={handleMonsterValue}
-                inputProps={{ sx: { p: 0.5, textAlign: 'center' } }}
-              />
-
-              <Typography variant="subtitle1" marginLeft={0.5}>
-                / {mapInfo.number_of_monster * 48}
-              </Typography>
-            </Box>
-          </Box>
-
-          <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, mb: 1 }}>
-            <Typography variant="subtitle1" marginLeft={0.5}>
-              6분당 경험치 획득량:{' '}
-              {sixMinutesMonsterValue * mapInfo.monster_experience}
-            </Typography>
-          </Box>
-
-          <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, mb: 1 }}>
-            <Typography variant="subtitle1" marginLeft={0.5}>
-              6분당 메소 획득량:{' '}
-              {sixMinutesMonsterValue * mapInfo.monster_money}
-            </Typography>
-          </Box>
+          <CardValueWithLabelUI
+            label="6분당 메소 획득량:"
+            value={sixMinutesMonsterValue * mapInfo.monster_money}
+          />
         </CardContent>
       </Box>
     </Card>
