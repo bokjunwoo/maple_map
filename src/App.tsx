b793d44ba3aa72466607a21000f2d0d9490232a2
JUNU <bokjunwoo@gmail.com>
2024-02-03 23:55:57 +0900

    feat: feat: App 컴포넌트에서 level을 useState로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import AppLayout from './components/UI/Layout/AppLayout';
 import ExpSetting from './components/ExpSetting';
 import MapSelect from './components/MapSelect';
 import { useState } from 'react';
+import LevelSetting from './components/LevelSetting';
 
 const App = () => {
+  const [level, setLevel] = useState(260);
   const [expIncrease, setExpIncrease] = useState(100);
 
   return (
     <AppLayout>
+      <LevelSetting level={level} setLevel={setLevel} />
       <ExpSetting expIncrease={expIncrease} setExpIncrease={setExpIncrease} />
-      <MapSelect />
+      <MapSelect level={level} />
     </AppLayout>
   );
 };
